0ab0bf1713a0ad2e00c19705ffaa5167cc707257
Mark Mossberg <mark.mossberg@gmail.com>
2020-11-01 17:42:30 -0500

    setup.py: More basic info

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools
 setuptools.setup(
     name='dawtool',
+    version='0.0.1',
+    author='Mark Mossberg',
     install_requires=['pytest', 'hexdump', 'scipy'],
     entry_points={
         "console_scripts": ['dawtool=dawtool.__main__:main']
