92776b0b9fbef1d8159e4b41ed7315e159fe6a70
Mark Mossberg <mark.mossberg@gmail.com>
2020-11-16 12:16:44 -0500

    setup: Enforce Python 3.7+
    This is because we use dataclasses.

--- README.md
+++ README.md
@@ -55,6 +55,8 @@ $ dawtool -m my-dj-mix.als
 
 ## Installation
 
+dawtool requires Python 3.7 or greater.
+
 ```
 git clone https://github.com/offlinemark/dawtool
 pip install dawtool
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setuptools.setup(
     name='dawtool',
     version='0.0.1',
     author='Mark Mossberg',
+    python_requires='>=3.7',
     install_requires=['pytest', 'hexdump', 'scipy'],
     entry_points={
         "console_scripts": ['dawtool=dawtool.__main__:main']
