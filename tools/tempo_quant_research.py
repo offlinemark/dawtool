376ed4415bf5edff8aee28744887b1a74275d662
Mark Mossberg <mark.mossberg@gmail.com>
2020-11-16 18:59:44 -0500

    tools: Show output in comment

--- tools/tempo_quant_research.py
+++ tools/tempo_quant_research.py
@@ -6,6 +6,22 @@ Research tool for brute forcing the tempo quantization of a DAW.
 - Compute the time elapsed for practical candidate temo quantizations (the
   first 13ish powers of 2)
 - See which candidate matches the DAW
+
+Output:
+
+1 4.0
+2 3.333333333333333
+4 3.038095238095238
+8 2.9014874014874015
+16 2.8360648088301073    // Ableton Live (2.836)
+32 2.8040828330769956
+64 2.788274755533614
+128 2.7804164809124314
+256 2.776498786929771
+512 2.7745428009136432   // FL Studio? (2.77)
+1024 2.7735655231583323
+2048 2.7730770630944246
+4096 2.7728328777659423
 """
 
 import numpy as np
