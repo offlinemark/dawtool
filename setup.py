f7300951602e0d7944283fbdb36cd30d9e1901a7
Mark Mossberg <mark.mossberg@gmail.com>
2020-11-17 19:42:24 -0500

    Fix package installation, add tests (#3)
    * Test cli works
    
    Remove editable install because it hides the fact the fact that there
    is a setup.py bug - we forgot the packages=
    
    * tmp disable tests
    
    * Reenable tests and fix setup.py
    
    * Use -h so ret code is zero
    
    * Tweak wording
    
    No longer testing just the lib - also testing that the cli works

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     author='Mark Mossberg',
     python_requires='>=3.7',
     install_requires=['pytest', 'hexdump', 'scipy'],
+    packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": ['dawtool=dawtool.__main__:main']
     }
