2cc30e098fefc7dbb5daaa2c9e6ff0a3289ee2c1
Mark Mossberg <mark.mossberg@gmail.com>
2020-11-18 22:40:44 -0500

    tests: fix tests for format_time

--- tests/test_format_time.py
+++ tests/test_format_time.py
@@ -19,13 +19,13 @@ def test_format_time_min_hour_fmt():
     assert format_time(60, True) == '00:01:00'
 
 def test_format_time_min_precise():
-    assert format_time(60, precise=True) == '01:00.000'
+    assert format_time(60, precise=True) == '01:00.00000'
 
 def test_format_time_hour_precise():
-    assert format_time(60*60, precise=True) == '60:00.000'
+    assert format_time(60*60, precise=True) == '60:00.00000'
 
 def test_format_time_hour_hour_fmt_precise():
-    assert format_time(60*60, precise=True) == '60:00.000'
+    assert format_time(60*60, precise=True) == '60:00.00000'
 
 def test_hours_pad():
     assert format_time(60*60, hours_fmt=True, hours_pad=False) == '1:00:00'
@@ -40,7 +40,7 @@ def test_hours_pad():
 
 
 def test_precise():
-    assert format_time(59.1, precise=True) == '00:59.100'
+    assert format_time(59.1, precise=True) == '00:59.10000'
     assert format_time(59.1) == '00:59'
-    assert format_time(60.1, precise=True) == '01:00.100'
+    assert format_time(60.1, precise=True) == '01:00.10000'
     assert format_time(60.1) == '01:00'
