c3aabeb8ab4178aa0fedc77c812e5fd62359b0bc
Mark Mossberg <mark.mossberg@gmail.com>
2020-11-22 19:27:22 -0500

    license: Relicense under dual license
    This respects the possible interpretation of the .flp parser as a
    derivative work of existing GPL .flp parsers, while allowing the
    rest of the codebase to be used with less restrictions.

--- dawtool/daw/flstudio.py
+++ dawtool/daw/flstudio.py
@@ -1,8 +1,19 @@
-"""
-Largely based off of LMMS/PyDaw, but with some new events they didn't have.
+# This file alone is licensed under the GPL v3.
 
-TODO: where is the flp format version? check against it?
-"""
+# Copyright (C) 2020 Mark Mossberg
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from ..project import Project
 from ..marker import Marker
