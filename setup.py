1d86b267ef81119efc6655fbffd4794d13d69c0f
Mark Mossberg <mark.mossberg@gmail.com>
2021-04-24 11:35:26 +0200

    add dep

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     version='0.0.1',
     author='Mark Mossberg',
     python_requires='>=3.7',
-    install_requires=['pytest', 'hexdump', 'scipy'],
+    install_requires=['pytest', 'hexdump', 'scipy', 'mido'],
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": ['dawtool=dawtool.__main__:main']
